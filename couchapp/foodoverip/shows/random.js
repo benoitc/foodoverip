437608319545781e2ef1dbc55d4d318a046540e0
benoitc <bchesneau@gmail.com>
2011-12-20 00:06:10 +0100

    improve template. & fix random

--- couchapp/foodoverip/shows/random.js
+++ couchapp/foodoverip/shows/random.js
@@ -10,14 +10,35 @@ function(doc, req) {
 
 
     var templates = ddoc.templates;
-   
-    photo_uri = path.rewrite() + "/photo/" + doc._id.replace("/", "%2F"); 
+    var docid = doc._id.replace("/", "%2F");
 
+   
+    var photo_uri = path.rewrite() + "/photo/" + docid;
+    var profile_uri  = path.rewrite() + "/avatar/" + docid;
+
+    var coords = null,
+        is_location = false;
+    if (doc.source.geo) {
+        is_location = true;
+        coords = { 
+            "lon": doc.source.geo.coordinates[0],
+            "lat": doc.source.geo.coordinates[1]
+        };
+    }
+
+    var uri = path.absolute(path.rewrite() + "/" + docid)
+
+    
     provides("html", function() {
         return Mustache.to_html(templates.food, {
             doc: doc,
+            root: path.rewrite(),
             photo_uri: photo_uri,
-            vhost: req.headers['x-couchdb-vhost-path'] 
+            profile_uri: profile_uri,
+            vhost: req.headers['x-couchdb-vhost-path'],
+            is_location: is_location,
+            coords: coords,
+            uri: uri     
         });
     });
 }
